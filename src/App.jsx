9e92a0b300b16925abd907256edfa60c183ac7f2
chrifrat1 <chrifrat1@gmail.com>
2023-08-03 20:29:08 +0200

    navigation bar styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-//import './categories.styles.scss'
-//import './components/category-item/category-item.component'
-//import CategoryItem from './components/category-item/category-item.component'
-//import Categories from './components/directory/directory.component'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+//import './App.css'
 import Home from './components/routes/home/home.component' //home component import
-import {Routes, Route, Outlet} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
+import Navigation from './components/routes/navigation/navigation.component'
+import './index.css'
 
-const Navigation =()=>{
-  return(
-    <div>
-        <div>
-            <h1>NAVIGATION BAR</h1>
-        </div>
-        <Outlet/>
-    </div>
-  )
-}
 
 const App=()=> {
+
+  const Shop =()=>{
+    return(
+      <div>
+        <h1>SHOP</h1>
+      </div>
+    )
+  }
 
   return (  //routes initialization path and element (component) 
     <Routes>
       <Route path='/' element={<Navigation/>}>
-        <Route path="home" element={<Home/>}/>
+        <Route path="" element={<Home/>}/>
+        <Route path="shop" element={<Shop/>}/>
       </Route>
     </Routes>
     
