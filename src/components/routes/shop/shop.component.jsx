113d030c57c972229594b3f9ffbf03287fb45ffd
chrifrat1 <chrifrat1@gmail.com>
2023-08-10 22:23:21 +0200

    creation of Context to access products

--- src/components/routes/shop/shop.component.jsx
+++ src/components/routes/shop/shop.component.jsx
@@ -1,9 +1,13 @@
-import SHOP_DATA from '../../../shop-data.json'
+//import SHOP_DATA from '../../../shop-data.json'
+import { useContext } from "react";
+import { ProductsContext } from "../../../contexts/products.context"; //import the products from the contect
 
 const Shop=()=>{
+
+    const {products}=useContext(ProductsContext)  //assign the products to const products
     return(
         <div>
-            {SHOP_DATA.map(({id,name})=>(
+            {products.map(({id,name})=>( //map products with id and name
                 <div key={id}>
                     <h1>{name}</h1>
                 </div>
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from './contexts/user.context'  //user context. allows specified component to access data
+import { ProductsProvider} from './contexts/products.context.jsx';
 
 //import './index.css'
 
@@ -10,7 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render( //nest the app into
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
