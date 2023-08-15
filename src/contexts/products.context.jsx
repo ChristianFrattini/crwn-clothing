ddb730edba0328b0cc7146df48582e2bc7979943
chrifrat1 <chrifrat1@gmail.com>
2023-08-15 16:31:33 +0200

    add collection to firebase

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,13 +1,17 @@
-import { createContext } from "react";
-import  PRODUCTS from '../shop-data.json'
+import { createContext, useEffect } from "react";
+import  SHOP_DATA from '../shop-data'
 import { useState } from "react";
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.utils";
 
 export const ProductsContext = createContext({
     products: []
 })
 
 export const ProductsProvider=({children})=>{
-    const [products, setProducts]=useState(PRODUCTS)
+    const [products, setProducts]=useState([])
+
+  
+
     const value ={products}
     return (<ProductsContext.Provider value={value}>{children}</ProductsContext.Provider>)
 }
