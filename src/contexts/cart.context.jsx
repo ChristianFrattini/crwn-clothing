50d1b5b18ec3bfcc6240003cd2a78e569fbfbcad
chrifrat1 <chrifrat1@gmail.com>
2023-08-17 18:43:05 +0200

    reducers - cart reducer

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,6 +1,8 @@
 import { createContext } from "react";
 import { useState } from "react";
 import { useEffect } from "react";  //use effect is deployed to detect the change of state of Cart items and update the final quantity
+import { useReducer } from "react"; 
+import {createAction} from '../utils/reducer/reducer.utils' //optimazion to avoid typing errors
 
 const addCartItem =(cartItems, productToAdd)=>{
 
@@ -48,8 +50,39 @@ export const CartContext = createContext({
     cartTotal: 0
 })
 
+const CART_ACTION_TYPES={
+    SET_CART_ITEMS: 'SET_CART_ITEMS',
+    SET_IS_CART_OPEN: 'SET_IS_CART_OPEN'
+}
+
+const INITIAL_STATE={
+    isCartOpen: false,
+    cartItems:[],
+    cartCount:0,
+    cartTotal:0,
+}
+
+const cartReducer=(state, action)=>{
+    const {type, payload}=action;
+
+    switch(type){
+        case CART_ACTION_TYPES.SET_CART_ITEMS:
+            return {
+                ...state,
+                ...payload
+            }
+        case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+            return {
+                ...state,
+                isCartOpen: payload,
+            }    
+        default:
+            throw new Error(`unhandled type  of ${type} in cartReducer`)
+    }
+}
+
 export const CartProvider=({children})=>{
-    const[isCartOpen,setIsCartOpen]=useState(false)
+    /*const[isCartOpen,setIsCartOpen]=useState(false)
     const [cartItems, setCartItems]=useState([])
     const [cartCount,setCartCount]=useState(0);
     const [cartTotal,setCartTotal]=useState(0);
@@ -62,21 +95,43 @@ export const CartProvider=({children})=>{
     useEffect(()=>{ //use effect for total price
         const newCartTotal=cartItems.reduce((total, cartItem)=> total + cartItem.quantity*cartItem.price, 0)
         setCartTotal(newCartTotal)
-    },[cartItems]) //this method is run every time something changes in the cartItems array
+    },[cartItems]) //this method is run every time something changes in the cartItems array*/
+
+    const [{cartItems, isCartOpen, cartCount,cartTotal}, dispatch]=useReducer(cartReducer,INITIAL_STATE)
+
+
+
+    const updateCartItemReducer=(newCartItems)=>{ //the function takes the cart functions and dispatches
+        const newCartTotal=newCartItems.reduce((total, cartItem)=> total + cartItem.quantity*cartItem.price, 0)
+        const newCartCount=newCartItems.reduce((total, cartItem)=> total + cartItem.quantity,0)
+
+        dispatch({type: CART_ACTION_TYPES.SET_CART_ITEMS, payload:{cartItems: newCartItems, cartTotal: newCartTotal, cartCount:newCartCount}})
+        
+    }
+
+
+
 
     const addItemToCart=(productToAdd)=>{ // function that gets triggered every time the user clicks on add to cart
         //console.log(productToAdd)
-        setCartItems(addCartItem(cartItems, productToAdd));
+        const newCartItems=addCartItem(cartItems, productToAdd);
+        updateCartItemReducer(newCartItems)
     }
 
     const removeItemFromCart=(cartItemToRemove)=>{ // function that gets triggered every time the user clicks on add to cart
         //console.log(productToAdd)
-        setCartItems(removeCartItem(cartItems, cartItemToRemove));
+        const newCartItems=removeCartItem(cartItems, cartItemToRemove);
+        updateCartItemReducer(newCartItems)
     }
 
     const clearItemFromCart=(cartItemToClear)=>{ // function that gets triggered every time the user clicks on add to cart
         //console.log(productToAdd)
-        setCartItems(clearCartItem(cartItems, cartItemToClear));
+        const newCartItems=clearCartItem(cartItems, cartItemToClear);
+        updateCartItemReducer(newCartItems)
+    }
+
+    const setIsCartOpen=(bool)=>{
+        dispatch({type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload:bool})
     }
 
     const value={isCartOpen,setIsCartOpen, addItemToCart, cartItems, cartCount, removeItemFromCart, clearItemFromCart, cartTotal}
