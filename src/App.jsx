6ee9a646a5ad163ec0aab75f72547d1309bc6af8
chrifrat1 <chrifrat1@gmail.com>
2023-08-18 20:01:44 +0200

    redux - store setup and create user reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+//import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
@@ -8,12 +8,30 @@ import Navigation from './components/routes/navigation/navigation.component'
 import Authentication from './components/routes/authentication/authentication.component'
 import Shop from './components/routes/shop/shop.component'
 import Checkout from './components/routes/checkout/checkout.component'
+import { useDispatch } from 'react-redux'
 
 import './index.css'
 
-const App=()=> {
+import { useState, useEffect } from "react";
+import { onAuthStateChangedListener,signOutUser, createUserDocumentFromAuth } from "./utils/firebase/firebase.utils";
+import { setCurrentUser } from './store/user/user.action'
 
-  
+const App=()=> {
+  const dispatch=useDispatch();
+
+  useEffect(()=>{ //use effect is run only once
+        
+    const unsubscribe = onAuthStateChangedListener((user)=>{
+        if(user){ //create a user document only if a user comes through, otherwise setcurrentuser
+            createUserDocumentFromAuth(user) //creates a doc when user logs in
+            
+        }
+        dispatch(setCurrentUser(user));
+        //console.log(user);
+
+    });  //passes the callback function as the second value 'callback' in firebase code
+    return unsubscribe;
+},[])
 
   return (  //routes initialization path and element (component) 
     <Routes>
