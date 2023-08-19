d59397e62301d18e3ba81325ae2f6a6d3655eeb1
chrifrat1 <chrifrat1@gmail.com>
2023-08-19 18:31:58 +0200

    enhancement of store middleware

--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,21 @@ import logger from 'redux-logger'
 
 import { rootReducer } from './root-reducer'
 
-const middleWare=[logger] //whenever an action is dispatched. before it hits the reducer it has to go through the middleware
+const loggerMiddleware=(store)=>(next)=>(action)=>{
+    if(!action.type){
+        return next(action);
+    }
+
+    console.log('type:', action.type)
+    console.log('payload:', action.payload)
+    console.log('currentState:', store.getState())
+
+    next(action);  
+    
+    console.log('next state: ', store.getState())
+}
+
+const middleWare=[loggerMiddleware] //whenever an action is dispatched. before it hits the reducer it has to go through the middleware
 const composeEnhancers=compose(applyMiddleware(...middleWare)) 
 
 //root reducers. core reducer that includes all other smaller reducers
