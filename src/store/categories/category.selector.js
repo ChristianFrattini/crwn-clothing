7f0012913edf3f32e2e55e3ca69044eb63a6fff1
chrifrat1 <chrifrat1@gmail.com>
2023-08-21 18:18:56 +0200

    reselect library - prevent re-render

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,5 +1,16 @@
-export const selectCategoriesMap =(state)=>state.categories.categories.reduce((acc, category)=>{
-    const {title, items}=category;
-    acc[title.toLowerCase()]=items
-    return acc
-  },{})
+import { createSelector } from "reselect";
+
+const selectCategoryReducer=(state)=>state.categories
+
+export const selectCategories=createSelector(
+    [selectCategoryReducer],
+    (categoriesReducer)=> categoriesReducer.categories
+)
+
+export const selectCategoriesMap = createSelector([selectCategories],  //as long as the categories array doesnt change then do not rerender
+    (categories)=>categories.reduce((acc, category)=>{
+        const {title, items}=category;
+        acc[title.toLowerCase()]=items
+        return acc
+      },{})
+    )
