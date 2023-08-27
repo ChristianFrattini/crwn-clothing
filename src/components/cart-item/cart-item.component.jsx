8660c14b41867b39ac8815202f00c67a9028be2a
chrifrat1 <chrifrat1@gmail.com>
2023-08-27 11:23:50 +0200

    update 1

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -8,7 +8,7 @@ const CartItem=({cartItem})=>{
             <img src={imageUrl} alt={`${name}`}/>
             <div className='item details'>
                 <span className='name'>{name}</span>
-                <span className='price'>{quantity}x${price}</span>
+                <span className='price'>{quantity}x£{price}</span>
             </div>
         </div>
     )
